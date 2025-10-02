16af6755557f3750296213473a03013dc475e8e2
David Porter <contact@davidporter.id.au>
2025-10-02 00:01:44 -0700

    adding field for active/active use-case (#224)
    Signed-off-by: David Porter <david.porter@uber.com>

--- thrift/shared.thrift
+++ thrift/shared.thrift
@@ -1209,6 +1209,9 @@ struct UpdateDomainResponse {
 struct FailoverDomainRequest {
  10: optional string domainName
  20: optional string domainActiveClusterName
+ // only applicable to active-active domains where 
+ // specific cluster-attributes are being failed over
+ 30: optional ActiveClusters activeClusters
 }
 
 struct FailoverDomainResponse {
